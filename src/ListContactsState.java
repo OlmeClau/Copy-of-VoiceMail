4581601101a5b2f7e0f96e9fdad977feb9de7f06
milenka <milenkaclaure18@gmail.com>
2016-04-23 19:14:13 +0200

    contact list usin stateInterface

---
+++ src/ListContactsState.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListContactsState implements ConnectionState {
+	List<Contact> contacts= new ArrayList<Contact>();;
+	 public void dial(String state, Connection con)
+	   {
+		 
+		 //Contact new_one = new Contact(name, number);
+		 //contacts.add(new_one);
+	      
+	   }
+	 
+}
